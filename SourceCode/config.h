7d8d1963abf6e2f3be5f1f61e8fb0fd2cfedf48c
Harry Han <hby@itfls.com>
2015-05-21 02:26:35 +0900

    增加了对 BST-V51 开发板的支持
    加入了 BST-V51 开发板的接口配置，提示了要求使用的晶振频率以及指令周期长度。

--- SourceCode/config.h
+++ SourceCode/config.h
@@ -13,7 +13,7 @@
 #define LOW                              0                      //低电平
 /*电平定义结束*/
 
-/*1602液晶显示屏配置开始*/
+/*1602液晶显示屏配置开始
 #define LCD1602_FUNCTION_BACKLIGHT       FUNCTION_ENABLED       //背光功能
 #define LCD1602_PIN_BACKLIGHT            P2^3                   //背光引脚
 #define LCD1602_PIN_ENABLE               P2^2                   //使能引脚
@@ -22,13 +22,30 @@
 #define LCD1602_PORT_BUS                 P0                     //总线端口
 #define LCD1602_STATUS_BACKLIGHT_OFF     HIGH                   //背光关
 #define LCD1602_STATUS_BACKLIGHT_ON      LOW                    //背光开
-/*1602液晶显示屏配置结束*/
+/*1602液晶显示屏配置结束
 
-/*键盘配置开始*/
+/*键盘配置开始
 #define KEYBOARD_PORT_SCAN               P1                     //扫描端口
-/*键盘配置结束*/
+/*键盘配置结束
 
 /**
  * 以上为 RF 开发板 + LCD1602 时的配置
- * 晶振频率：  22.1184MHz
+ * 晶振频率：  22.1184MHz 12T运行（实际内部频率22.1184MHz）
+ */
+ 
+ /*1602液晶显示屏配置开始*/
+#define LCD1602_FUNCTION_BACKLIGHT       FUNCTION_DISABLED      //背光功能
+#define LCD1602_PIN_ENABLE               P2^5                   //使能引脚
+#define LCD1602_PIN_READWRITE            P1^1                   //读写选择引脚
+#define LCD1602_PIN_REGISTER             P1^0                   //命令数据选择引脚
+#define LCD1602_PORT_BUS                 P0                     //总线端口
+/*1602液晶显示屏配置结束*/
+
+/*键盘配置开始*/
+#define KEYBOARD_PORT_SCAN               P3                     //扫描端口
+/*键盘配置结束*/
+
+/**
+ * 以上为 BTS-V51 开发板 + LCD1602 时的配置
+ * 晶振频率：  11.0592MHz 6T运行（实际内部频率22.1184MHz）
  */
