78128bd7e73f93d45220cd1b317fa3481322b72e
Harry Han <hby@itfls.com>
2015-05-21 02:29:40 +0900

    实现了输入与显示金额功能
    对应地修改了函数声明，实现了通过键盘获取类似“123.45”格式的金额并显示在1602液晶上的功能。
    TODO：（原）增加函数注释；实现串口通信

--- SourceCode/main.c
+++ SourceCode/main.c
@@ -9,19 +9,59 @@
 void main(void)
 {
     extern void lcd1602Backlight(bit status);
-    extern void lcd1602Display(unsigned char line, unsigned char row, unsigned char *content, unsigned char contentLenth);
+    extern void lcd1602Display(unsigned char line, unsigned char row, unsigned char* content);
     extern void lcd1602Init(void);
-    extern unsigned char* keyboardGetKey(void);
+    extern unsigned char keyboardGetKey(void);
+    extern char* keyboardToString(unsigned char scanCode);
+    extern void lcd1602RollRight(void);
+    extern void lcd1602Clear(void);
+    extern void lcd1602WriteCommand(unsigned char lcd1602Command, bit waitBusy);
 
-    unsigned char* keyNumber;
+    unsigned char keyCode = 0xFF,
+                  price[7],
+                  digitOfDecimalPart = 0,
+                  digitOfIntegerPart = 0;
+    bit hasDot = 0;
 
     lcd1602Init();
+    price[0] = '\0';
+    
+    #if LCD1602_FUNCTION_BACKLIGHT
     lcd1602Backlight(LCD1602_STATUS_BACKLIGHT_ON);
+    #endif
 
     while (1) {
-        keyNumber = keyboardGetKey();
-        if (keyNumber != NULL) {
-            lcd1602Display(1, 1, keyNumber, 1);
+        keyCode = keyboardGetKey();
+        if (keyCode != 0xFF) {
+            if (keyCode == 0x0B) {
+                lcd1602Clear();
+                hasDot = 0;
+                digitOfDecimalPart = 0;
+                digitOfIntegerPart = 0;
+                price[0] = '\0';
+                continue;
+            }
+            
+            if (strlen(price) < 6 && digitOfDecimalPart < 2) {
+                if (keyCode == 0x0A) {
+                    if (!hasDot){
+                        strcat(price, ".");
+                        hasDot = 1;
+                    }
+                } else {
+                    if (digitOfIntegerPart >= 3 && !hasDot) {
+                        continue;
+                    }
+                    strcat(price, keyboardToString(keyCode));
+                    if (hasDot) {
+                        digitOfDecimalPart++;
+                    } else {
+                        digitOfIntegerPart++;
+                    }
+                }
+
+                lcd1602Display(1, 1, price);
+            }
         }
     }
 }
