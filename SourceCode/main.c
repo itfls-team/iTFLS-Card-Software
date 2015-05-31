35f9981bd4b89746355e3ea2a96879d4e94afa19
Harry Han <hby@itfls.com>
2015-06-01 00:52:30 +0900

    对应地更新了主函数
    已经实现输入金额，读取卡号，串口发送相关数据的功能。

--- SourceCode/main.c
+++ SourceCode/main.c
@@ -15,61 +15,94 @@ void main(void)
     extern unsigned char* keyboardToString(unsigned char scanCode);
     extern void lcd1602Clear(void);
     extern void serialPortInit(void);
-    extern void serialPortTransmit(unsigned char* dataToTransmit);
+    extern void serialPortTransmit(unsigned char* dataToTransmit, bit isString, unsigned char dataLenth);
     extern unsigned char* serialPortReceive(void);
+    extern void MFRC500ChipStart(void);
+    extern void MFRC500GetCardSerialNumber(unsigned char* cardSN);
+    extern void buzzerShortBeep(void);
+    extern void buzzerLongBeep(void);
+    extern void MFRC500ChipHalt(void);
 
     unsigned char keyCode = 0xFF,
-                  price[7],
+                  priceString[7],
                   digitOfDecimalPart = 0,
-                  digitOfIntegerPart = 0;
+                  digitOfIntegerPart = 0,
+                  cardSerialNumber[5];
+    unsigned char xdata dataToSend[14];
+    union {
+        float f;
+        unsigned char c[4];
+    } price;
     bit hasDot = 0;
 
     lcd1602Init();
+    lcd1602Backlight(LCD1602_STATUS_BACKLIGHT_ON);
     serialPortInit();
     EA = 1;
-    price[0] = '\0';
-    
-    #if LCD1602_FUNCTION_BACKLIGHT
-    lcd1602Backlight(LCD1602_STATUS_BACKLIGHT_ON);
-    #endif
+    priceString[0] = '\0';
+    dataToSend[0] = '\0';
+    buzzerShortBeep();
+    lcd1602Display(1, 1, "Ready!");
 
     while (1) {
-        keyCode = keyboardGetKey();
-        if (keyCode != 0xFF) {
-            if (keyCode == 0x0B) {
-                unsigned char priceJsonString[20];
-                sprintf(priceJsonString, "{\"price\":\"%s\"}\n", price);
-                lcd1602Clear();
-                serialPortTransmit(priceJsonString);
-                lcd1602Display(1,1,serialPortReceive());
-                hasDot = 0;
-                digitOfDecimalPart = 0;
-                digitOfIntegerPart = 0;
-                price[0] = '\0';
-                continue;
-            }
-            
-            if (strlen(price) < 6 && digitOfDecimalPart < 2) {
-                if (keyCode == 0x0A) {
-                    if (!hasDot){
-                        strcat(price, ".");
-                        hasDot = 1;
-                    }
-                } else {
-                    if (digitOfIntegerPart >= 3 && !hasDot) {
-                        continue;
-                    }
-                    strcat(price, keyboardToString(keyCode));
-                    if (hasDot) {
-                        digitOfDecimalPart++;
-                    } else {
-                        digitOfIntegerPart++;
-                    }
+        while (1) {
+            keyCode = keyboardGetKey();
+            if (keyCode != 0xFF) {
+                if (keyCode == 0x0B) {
+                    lcd1602Clear();
+                    hasDot = 0;
+                    digitOfDecimalPart = 0;
+                    digitOfIntegerPart = 0;
+                    break;
                 }
 
-                lcd1602Clear();
-                lcd1602Display(1, 1, price);
+                if (strlen(priceString) < 6 && digitOfDecimalPart < 2) {
+                    if (keyCode == 0x0A) {
+                        if (!hasDot){
+                            strcat(priceString, ".");
+                            hasDot = 1;
+                        }
+                    } else {
+                        if (digitOfIntegerPart >= 3 && !hasDot) {
+                            continue;
+                        }
+                        strcat(priceString, keyboardToString(keyCode));
+                        if (hasDot) {
+                            digitOfDecimalPart++;
+                        } else {
+                            digitOfIntegerPart++;
+                        }
+                    }
+
+                    lcd1602Clear();
+                    lcd1602Display(1, 1, priceString);
+                }
             }
         }
+
+        lcd1602Display(1, 1, priceString);
+        lcd1602Display(2, 1, "Please Read Card");
+
+        MFRC500ChipStart();
+        MFRC500GetCardSerialNumber(cardSerialNumber);
+        buzzerShortBeep();
+        MFRC500ChipHalt();
+        
+        price.f = atof(priceString);
+        priceString[0] = '\0';
+        dataToSend[0] = 0x69; //i
+        dataToSend[1] = 0x54; //T
+        dataToSend[2] = price.c[3];
+        dataToSend[3] = price.c[2];
+        dataToSend[4] = price.c[1];
+        dataToSend[5] = price.c[0];
+        dataToSend[6] = 0x46; //F
+        memcpy(&dataToSend[7], &cardSerialNumber[1], 4);
+        dataToSend[11] = 0x4C; //L
+        dataToSend[12] = 0x53; //S
+        serialPortTransmit(dataToSend, 0, 13);
+        dataToSend[0] = '\0';
+        lcd1602Clear();
+        lcd1602Display(1, 5, "Success!");
     }
 }
